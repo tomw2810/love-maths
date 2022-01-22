2382f09c71bd283e1dfd54f957b697a6818fc469
Tom Woodhouse <tomwoodhouse1991@gmail.com>
2022-01-22 17:00:47 +0000

    js added to

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,20 @@
+//Wait for the DOM to finish loading
+//Get the button elements and add event listerners to them
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName('button');
+
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You cliked submit");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+})
+
 function runGame() {
 
 }
