bb33b70fcb070781b214f481cb20644148091473
Tom Woodhouse <tomwoodhouse1991@gmail.com>
2022-01-22 16:53:02 +0000

    js started

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,31 @@
-console.log('yello');
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
