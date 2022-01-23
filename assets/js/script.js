0529ec39a0722400847b6d1b74cfc632ec8f3901
Tom Woodhouse <tomwoodhouse1991@gmail.com>
2022-01-23 12:33:52 +0000

    3

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,8 +34,21 @@ function checkAnswer() {
 
 }
 
+/**
+ * gets the operands (the numbers) and the operator (plus, minus etc)
+ * directly from the DOM and returns the correct answer.
+ */
 function calculateCorrectAnswer() {
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById('operator').innerText;
 
+    if (operator === '+') {
+        return [operand1 + operand2, "addition"];
+    } else {
+        alert(`Unimpleneted operator ${operator}`);
+        throw `Unimplented operator ${operator}. Aborting!`;
+    }
 }
 
 function incementScore() {
